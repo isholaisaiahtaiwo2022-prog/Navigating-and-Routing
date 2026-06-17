91f33c14fbc019e9fbce53f39ea9a223c2f77ab6
isholaisaiahtaiwo2022-prog <isholaisaiahtaiwo2022@gmail.com>
2026-06-17 16:16:36 -0700

    First Commit

---
+++ routingandnavigation/lib/login.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'dashboard_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Login"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const DashboardScreen(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
